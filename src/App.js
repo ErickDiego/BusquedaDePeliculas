6218b16d22370db2ea73633b22f231a195b42303
Erick Neculhueque Marihual <e.neculhueque@alumnos.duoc.cl>
2020-08-24 22:56:38 -0400

    Consumo de API Pelicula

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Title} from './Components/Title'
+import {FormularioDeBusqueda} from  './Components/FormularioDeBusqueda'
+
+import 'bulma/css/bulma.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+state = {result: []}
+
+_handleResults = (result) => {
+  this.setState({result})
+}
+
+_renderResults(){
+  const {result} = this.state
+
+  return result.map(movie => {
+      return <p key={movie.imdbID}>{movie.Title}</p>
+  })
+}
+
+render(){
+    return (
+      <div className="App">
+          <Title>Busqueda de Peliculas</Title>
+      <div className="FormBusqueda-wrapper">
+            <FormularioDeBusqueda onResults={this._handleResults}/>
+      </div>
+
+      {this.state.result.lenght === 0
+        ? <p>-sin resultados</p>
+        : this._renderResults()
+      }
+
+      </div>
+    );
+  }
 }
 
 export default App;
